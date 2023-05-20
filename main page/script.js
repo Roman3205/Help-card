4f62af30cb8f8145bbf6df26083c44a3133be57f
Roman  Garbuzov <garbromannik@gmail.com>
2023-05-20 14:55:49 +0300

    commit

---
+++ main page/script.js
@@ -0,0 +1,7 @@
+// cookie //
+let cookie = document.querySelector(`.boxcook`)
+let cookiebut = document.querySelector(`.butcook`)
+
+butcook.addEventListener(`click`, function() {
+    cookie.classList.add(`d-none`)
+})
